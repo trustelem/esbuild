da94956c0e5a20cf379b1e6f6a80f6e1b83aa37d
Georges Varouchas <gvarouchas@wallix.com>
2021-09-24 18:00:51 +0200

    add basic test loading node_modules from a zip file

---
+++ tstbuild/zip_test.go
@@ -0,0 +1,36 @@
+package tstbuild
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/trustelem/esbuild/pkg/api"
+	"github.com/trustelem/esbuild/pkg/cli"
+	"github.com/trustelem/esbuild/pkg/zipfs"
+)
+
+func TestApiZipFS(t *testing.T) {
+	checkEnv(t)
+
+	cwd, _ := os.Getwd()
+	root := filepath.Join(cwd, "node_modules")
+
+	zfs, err := zipfs.New("foo.zip", root, "node_modules/", cwd)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const output = "zip.js"
+	if _, err := os.Stat(output); err == nil {
+		os.Remove(output)
+	}
+
+	opts, _, _, _ := cli.UnitTestParseOptions([]string{"--bundle", "--minify", "--outfile=" + output, "app.jsx"})
+	opts.FS = zfs
+
+	result := api.Build(*opts)
+	if len(result.Errors) > 0 {
+		t.Fatalf("build failed\n%s", toJson(result.Errors, true))
+	}
+}
